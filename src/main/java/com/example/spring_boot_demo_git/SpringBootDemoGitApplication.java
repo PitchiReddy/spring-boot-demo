20e02e5f242dbef57bc9fa281770119dc1c02bbf
Pitchi <test@gmail.com>
2024-10-29 15:34:37 +0530

    first commit

---
+++ src/main/java/com/example/spring_boot_demo_git/SpringBootDemoGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_demo_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoGitApplication.class, args);
+	}
+
+}
